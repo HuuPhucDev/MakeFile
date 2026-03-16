0802067da13cfb03735050ce0563a9660830a525
huuphuc.vo <vohuuphuc26@gmail.com>
2026-03-17 00:11:24 +0700

    Make file for bare metal system

---
+++ MakeFile_HelloWork/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#define x 1
+
+void main()
+{
+   int a = x;
+   printf("a = %d", a);
+}
---
+++ MakeFile_HelloWorld1/Src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "sum.h"
+
+void main()
+{
+  int a=1, b=2;
+  printf ("a +b =%d", sum(a,b));
+}
